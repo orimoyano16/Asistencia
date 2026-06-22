10333284d57798db4eb26319f3d9bcb16b227fa9
orimoyano16 <orianamoyano@gmail.com>
2026-06-22 01:03:15 -0300

    Estructura inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './Componentes/Pages/home/home';
+import { Navbar } from './Componentes/Shared/navbar/navbar';
+import { Footer } from './Componentes/Shared/footer/footer';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'Home', component: Home },
+  { path: 'navbar', component: Navbar },
+  { path: 'footer', component: Footer},
+  
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './Componentes/Shared/navbar/navbar';
+import { Home } from './Componentes/Pages/home/home'
+import { Footer } from './Componentes/Shared/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Footer, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
